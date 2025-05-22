c365a85d4eced0fc87ee16fc1b42b93b5055bf6a
prisciliaking <prisciliakingchandra@gmail.com>
2025-05-22 10:15:01 +0700

    config firebase

--- S4-MAD-ALP/S4_MAD_ALPApp.swift
+++ S4-MAD-ALP/S4_MAD_ALPApp.swift
@@ -5,10 +5,21 @@
 //  Created by student on 22/05/25.
 //
 
+import FirebaseAppCheck
+import FirebaseCore
 import SwiftUI
 
 @main
 struct S4_MAD_ALPApp: App {
+
+    init() {
+        FirebaseApp.configure()
+        #if DEBUG
+            let providerFactory = AppCheckDebugProviderFactory()
+            AppCheck.setAppCheckProviderFactory(providerFactory)
+        #endif
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
--- S4-MAD-ALP/View/CanvasView.swift
+++ S4-MAD-ALP/View/CanvasView.swift
@@ -14,6 +14,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct CanvasView: View {
     @ObservedObject var viewModel: CanvasViewModel
