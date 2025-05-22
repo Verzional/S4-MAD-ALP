6229a378967c836bec218dc38ac16e054cbfaf08
Verzional <153180879+Verzional@users.noreply.github.com>
2025-05-22 09:02:36 +0700

    Initial Commit

---
+++ S4-MAD-ALP/S4_MAD_ALPApp.swift
@@ -0,0 +1,17 @@
+//
+//  S4_MAD_ALPApp.swift
+//  S4-MAD-ALP
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct S4_MAD_ALPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
